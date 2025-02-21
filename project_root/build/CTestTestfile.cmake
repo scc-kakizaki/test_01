78b89a8337dcbe979bb6d9c516a459f806252d75
scc-kakizaki <shun.kakizaki@scc-kk.co.jp>
2025-02-21 15:51:21 +0900

    mkdir build

--- project_root/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/shu-kakizaki/Desktop/work/test_01/project_root
-# Build directory: C:/Users/shu-kakizaki/Desktop/work/test_01/project_root/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("extern/googletest")

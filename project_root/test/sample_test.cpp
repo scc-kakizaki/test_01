79b479a4ec65469f48eff51317cbefc22098953e
scc-kakizaki <shun.kakizaki@scc-kk.co.jp>
2025-02-26 11:45:24 +0900

    Update sample_test.cpp

--- project_root/test/sample_test.cpp
+++ project_root/test/sample_test.cpp
@@ -5,3 +5,18 @@ TEST(SampleTest, TestFunction1)
 {
 	EXPECT_EQ(10, 10);
 }
+
+TEST(SampleTest, TestFunction2)
+{
+	EXPECT_EQ(1, 10);
+}
+
+TEST(SampleTest, TestFunction3)
+{
+	EXPECT_EQ(10, 10);
+}
+
+TEST(SampleTest, TestFunction4)
+{
+	EXPECT_EQ(5, 10);
+}
